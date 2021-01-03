4131a11dbccd9012e549b53bf862ce236247306f
Monzer Mohammad <72494044+DevMonzer@users.noreply.github.com>
2021-01-03 15:09:26 +0200

    added some js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
